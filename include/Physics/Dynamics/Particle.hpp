529e4014125a01067b8726480e8288357e6a067a
Krzysztof Lenarczyk <krzysztoflenarczyk06@gmail.com>
2025-04-19 21:22:44 +0200

    Implement how forces affect particles

--- include/Physics/Dynamics/Particle.hpp
+++ include/Physics/Dynamics/Particle.hpp
@@ -17,12 +17,13 @@ namespace nwtn {
         float damping;
 
         float inverseMass;
+        Vector2 forceAccum;
 
     public:
         // Constructors
-        Particle() : position(0, 0), velocity(0, 0), acceleration(0, 0), damping(0.995f), inverseMass(1.0f) {}
-        Particle(const Vector2& position, const Vector2& velocity, const Vector2& acceleration, float damping, float inverseMass)
-            : position(position), velocity(velocity), acceleration(acceleration), damping(damping), inverseMass(inverseMass) {}
+        Particle() : position(0, 0), velocity(0, 0), acceleration(0, 0), damping(0.995f), inverseMass(1.0f), forceAccum(0, 0) {}
+        Particle(const Vector2& position, const Vector2& velocity, const Vector2& acceleration, float damping, float inverseMass, const Vector2& forceAccum)
+            : position(position), velocity(velocity), acceleration(acceleration), damping(damping), inverseMass(inverseMass), forceAccum(forceAccum) {}
 
         // Getters and Setters
         Vector2 getPosition() const { return position; }
@@ -31,6 +32,7 @@ namespace nwtn {
         float getDamping() const { return damping; }
         float getInverseMass() const { return inverseMass; }
         float getMass() const;
+        Vector2 getForceAccum() const { return forceAccum; }
         void setPosition(const Vector2& newPosition) { position = newPosition; }
         void setPosition(float x, float y) { position.x = x; position.y = y; }
         void setVelocity(const Vector2& newVelocity) { velocity = newVelocity; }
@@ -40,6 +42,13 @@ namespace nwtn {
         void setDamping(float newDamping);
         void setInverseMass(float newInverseMass) { inverseMass = newInverseMass; }
         void setMass(float mass);
+        void setForceAccum(const Vector2& newForceAccum) { forceAccum = newForceAccum; }
+        void setForceAccum(float x, float y) { forceAccum.x = x; forceAccum.y = y; }
+
+        // Force accumulation
+        void addForce(const Vector2& force) { forceAccum += force; }
+        void addForce(float x, float y) { forceAccum.x += x; forceAccum.y += y; }
+        void clearAccumulator() { forceAccum.x = 0; forceAccum.y = 0; }
     };
 
 }
